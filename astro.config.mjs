b41a9e43e497cd7069912c8d22337c33919548df
Daniel Barajas <danbarajas7@gmail.com>
2026-05-26 00:51:26 -0500

    Connect Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,4 +13,10 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  output: 'server',
+  adapter: vercel({
+      webAnalytics: {
+        enabled: true,
+      },
+  }),
 });
