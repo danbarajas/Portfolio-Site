80d93bc54082ff2f0415ba1a9c832c5232516644
Daniel Barajas <danbarajas7@gmail.com>
2026-05-28 13:38:39 -0500

    Basic about tab design

--- src/data/vars.ts
+++ src/data/vars.ts
@@ -4,3 +4,7 @@ export const links = [
     { name: 'Projects', id: 'projects' },
     { name: 'Contact', id: 'contact' },
 ];
+
+export const frontendTags = ['React', 'Astro', 'Tailwind CSS', 'JavaScript', 'TypeScript'];
+export const backendTags = ['Node.js', 'C#', 'SQL', 'Firebase', 'PHP'];
+export const toolsTags = ['Unity', 'Figma'];
